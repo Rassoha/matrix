0d1373c5138f592f1ea9e36a960243a6e23bc8ff
Sergey Dribas <serdribas@gmail.com>
2024-10-17 16:38:00 +0300

    Working prog without clear memory

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 int main() {
   size_t m = 0;
   size_t n = 0;
-  int** t = matrix(m, n);
   std::cin >> m >> n;
+  if (!std::cin){
+    std::cerr <<"NOT A NUBMER!!!";
+    return 1;
+  }
+  int** t = matrix(m, n);
   std::cout << m << " " << n;
   std::cout << '\n';
+  enter(t,m, n);
+  std::cout << '\n';
+  out(t, m, n);
   clear(t, m);
 
 }
--- matrix.hpp
+++ matrix.hpp
@@ -1,5 +1,6 @@
-#ifdef MATRIX_HPP
+#ifndef MATRIX_HPP
 #define MATRIX_HPP
+#include <cstddef>
 int** matrix(size_t m, size_t n);
 void clear(int** t, size_t m);
 void enter(int** t, size_t m, size_t n);
