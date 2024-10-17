6bb535c191de3da38a2a3c2cba3c819721a9efd9
Sergey Dribas <serdribas@gmail.com>
2024-10-17 14:52:21 +0300

    Add funcion mATRIX.HPP

---
+++ matrix.hpp
@@ -0,0 +1,7 @@
+#ifdef MATRIX_HPP
+#define MATRIX_HPP
+int** matrix(size_t m, size_t n);
+void clear(int** t, size_t m);
+void enter(int** t, size_t m, size_t n);
+void out(const int* const* t, size_t m, size_t n);
+#endif
