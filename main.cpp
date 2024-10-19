606a0d8f7c437d694af7b4bf68a394b906733bda
Sergey Dribas <serdribas@gmail.com>
2024-10-19 12:25:19 +0300

    litle cg fix

--- main.cpp
+++ main.cpp
@@ -6,20 +6,20 @@ int main() {
 
   std::cin >> m >> n;
   if (!std::cin){
-    std::cerr <<"NOT A NUBMER!!!";
-    return 1;
+   std::cerr <<"NOT A NUBMER!!!";
+   return 1;
   }
   int** t = nullptr;
   try{
-    t=matrix(m,n);
+   t=matrix(m,n);
   }
   catch(std::bad_alloc& e) {
-    std::cerr << "Error"<<e.what()<<'\n';
-    return 1;
+   std::cerr << "Error" << e.what() << '\n';
+   return 1;
   }
   enter(t,m,n);
   out(t, m, n);
-  std::cout <<'\n';
+  std::cout << '\n';
   clear(t, m);
 
 }
--- matrix.cpp
+++ matrix.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
 #include "matrix.hpp"
 void clear(int** t, size_t m){
-  for(size_t i=0;i<m;i+=1){
-    delete[] t[i];
+  for(size_t i = 0; i < m; i += 1){
+   delete[] t[i];
   }
   delete[] t;
 }
 int** matrix(size_t m, size_t n){
   int ** t=new int*[m];
-  size_t created=0;
+  size_t created = 0;
   try{
-    for (;created<n;created+=1){
-      t[created]=new int[n];
+   for (; created < n; created += 1){
+    t[created] = new int[n];
     }
   }
-  catch(const std::bad_alloc &e){
-    clear(t,created);
-    throw;
+  catch(const std::bad_alloc & e){
+   clear(t,created);
+   throw;
   }
   return t;
 }
 void enter(int** t, size_t m, size_t n) {
   for (size_t i = 0; i < m; i += 1) {
-    for (size_t j = 0; j < n; j += 1) {
-      std::cin >> t[i][j];
-      if (!std::cin){
-        std::cerr<< "ENTER NOT A NUBMER!!!";
-        break;
-      }
+   for (size_t j = 0; j < n; j += 1) {
+    std::cin >> t[i][j];
+    if (!std::cin){
+     std::cerr << "ENTER NOT A NUBMER!!!";
+     break;
     }
   }
+ }
 }
 void out(const int* const* t, size_t m, size_t n) {
   for (size_t i = 0; i < m; i += 1) {
-    std::cout << t[i][0];
-    for (size_t j = 1; j < n; j += 1) {
-      std::cout << " " << t[i][j];
-    }
-    std::cout << "\n";
+   std::cout << t[i][0];
+   for (size_t j = 1; j < n; j += 1) {
+    std::cout << " " << t[i][j];
+   }
+   std::cout << "\n";
   }
 }
